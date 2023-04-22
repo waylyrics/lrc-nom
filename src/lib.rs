7c4d52f6d7363dbf19f2c0b8e6770d533832c0c2
poly000 <1348292515@qq.com>
2023-04-22 11:52:44 +0800

    do not panic on invalid second

--- src/lib.rs
+++ src/lib.rs
@@ -93,8 +93,7 @@ pub fn parse<'a>(lyric: &'a str, lf: &str) -> Result<Vec<LrcItem<'a>>, LrcParseE
                     _minute => {
                         for (_left_sq, minute, _semicon, sec, _right_sq) in tags {
                             let millisec = Decimal::from_str_exact(sec)
-                                .map_err(|_| LrcParseError::InvalidTimestamp(i))
-                                .unwrap()
+                                .map_err(|_| LrcParseError::InvalidTimestamp(i))?
                                 * dec!(1000);
                             let timestamp = minute
                                 .parse::<i64>()
