58916b6462ee3bacabfd181ac92cdcf178479e09
poly000 <1348292515a@gmail.com>
2024-04-15 02:25:46 +0800

    feat: parse_single for single line
    BREAKING CHANGE: `IrcItem::Lyric` now stores a `Vec<i64>`, previously we copy &str for multiple timestamps in single line, now we could distingush between repeated line and the elimated form.

--- src/lib.rs
+++ src/lib.rs
@@ -30,7 +30,7 @@ pub enum LrcMetadata<'a> {
 pub enum LrcItem<'a> {
     Metadata(LrcMetadata<'a>),
     /// Lyric text and timestamp in milliseconds without offset
-    Lyric(&'a str, i64),
+    Lyric(&'a str, Vec<i64>),
 }
 
 #[derive(Debug, Error)]
@@ -43,9 +43,7 @@ pub enum LrcParseError {
     InvalidOffset(usize),
 }
 
-pub fn parse<'a>(
-    lyric_lines: impl Iterator<Item = &'a str>,
-) -> Result<Vec<LrcItem<'a>>, LrcParseError> {
+pub fn parse_single(line: &str, line_num: usize) -> Result<Option<LrcItem<'_>>, LrcParseError> {
     use nom::{
         bytes::complete::{tag, take_until},
         multi::many1,
@@ -63,63 +61,64 @@ pub fn parse<'a>(
         tag("]"),
     )));
 
-    let mut lrc_items = Vec::new();
+    if line.trim().is_empty() {
+        return Ok(None);
+    }
 
-    for (i, line) in lyric_lines.filter(|l| !l.is_empty()).enumerate() {
-        let parse_result: IResult<&str, Vec<(&str, &str, &str, &str, &str)>> = tag_parser(line);
-        match parse_result {
-            Ok((text, tags)) => match tags[0] {
-                // `[:]` is considered as comment line
-                (_left_sq, "", _semicon, "", _right_sq) => continue,
-                (_left_sq, attr, _semicon, content, _right_sq) => match attr.trim() {
-                    "ar" => lrc_items.push(LrcItem::Metadata(LrcMetadata::Artist(content.trim()))),
-                    "al" => lrc_items.push(LrcItem::Metadata(LrcMetadata::Album(content.trim()))),
-                    "ti" => lrc_items.push(LrcItem::Metadata(LrcMetadata::Title(content.trim()))),
-                    "au" => {
-                        lrc_items.push(LrcItem::Metadata(LrcMetadata::Lyricist(content.trim())))
-                    }
-                    "length" => {
-                        lrc_items.push(LrcItem::Metadata(LrcMetadata::Length(content.trim())))
-                    }
-                    "by" => lrc_items.push(LrcItem::Metadata(LrcMetadata::Author(content.trim()))),
-                    "offset" => lrc_items.push(LrcItem::Metadata(LrcMetadata::Offset(
-                        content
-                            .trim()
-                            .parse()
-                            .map_err(|_| LrcParseError::InvalidOffset(i))?,
-                    ))),
-                    "re" => {
-                        lrc_items.push(LrcItem::Metadata(LrcMetadata::Application(content.trim())))
-                    }
-                    "ve" => {
-                        lrc_items.push(LrcItem::Metadata(LrcMetadata::AppVersion(content.trim())))
-                    }
-                    "#" => lrc_items.push(LrcItem::Metadata(LrcMetadata::Comment(content.trim()))),
-                    _minute if _minute.parse::<i64>().is_ok() => lrc_items.extend(
-                        tags.into_iter()
-                            .map(|(_left_sq, minute, _semicon, sec, _right_sq)| {
-                                let millisec = Decimal::from_str_exact(&sec.replace(':', "."))
-                                    .map_err(|_| LrcParseError::InvalidTimestamp(i))?
-                                    * dec!(1000);
-                                let timestamp = minute
-                                    .parse::<i64>()
-                                    .map_err(|_| LrcParseError::InvalidTimestamp(i))?
-                                    * 60
-                                    * 1000
-                                    + millisec
-                                        .to_i64()
-                                        .ok_or(LrcParseError::InvalidTimestamp(i))?;
+    let parse_result: IResult<&str, Vec<(&str, &str, &str, &str, &str)>> = tag_parser(line);
+    let (text, tags) = parse_result.map_err(|_| LrcParseError::NoTagInNonEmptyLine(line_num))?;
+    Ok(Some(match tags[0] {
+        // `[:]` is considered as comment line
+        (_left_sq, "", _semicon, "", _right_sq) => LrcItem::Metadata(LrcMetadata::Comment(text)),
+        (_left_sq, attr, _semicon, content, _right_sq) => match attr.trim() {
+            "ar" => LrcItem::Metadata(LrcMetadata::Artist(content.trim())),
+            "al" => LrcItem::Metadata(LrcMetadata::Album(content.trim())),
+            "ti" => LrcItem::Metadata(LrcMetadata::Title(content.trim())),
+            "au" => LrcItem::Metadata(LrcMetadata::Lyricist(content.trim())),
+            "length" => LrcItem::Metadata(LrcMetadata::Length(content.trim())),
+            "by" => LrcItem::Metadata(LrcMetadata::Author(content.trim())),
+            "offset" => LrcItem::Metadata(LrcMetadata::Offset(
+                content
+                    .trim()
+                    .parse()
+                    .map_err(|_| LrcParseError::InvalidOffset(line_num))?,
+            )),
+            "re" => LrcItem::Metadata(LrcMetadata::Application(content.trim())),
+            "ve" => LrcItem::Metadata(LrcMetadata::AppVersion(content.trim())),
+            "#" => LrcItem::Metadata(LrcMetadata::Comment(content.trim())),
+            _minute if _minute.parse::<i64>().is_ok() => {
+                let mut timestamps = Vec::with_capacity(tags.len());
+                for (_left_sq, minute, _semicon, sec, _right_sq) in tags {
+                    let millisec = Decimal::from_str_exact(&sec.replace(':', "."))
+                        .map_err(|_| LrcParseError::InvalidTimestamp(line_num))?
+                        * dec!(1000);
+                    let timestamp = minute
+                        .parse::<i64>()
+                        .map_err(|_| LrcParseError::InvalidTimestamp(line_num))?
+                        * 60
+                        * 1000
+                        + millisec
+                            .to_i64()
+                            .ok_or(LrcParseError::InvalidTimestamp(line_num))?;
+                    timestamps.push(timestamp);
+                }
+                LrcItem::Lyric(text, timestamps)
+            }
+            _ => return Ok(None), // ignores unrecognised tags
+        },
+    }))
+}
 
-                                Ok::<LrcItem<'_>, LrcParseError>(LrcItem::Lyric(text, timestamp))
-                            })
-                            .flatten(), // Errors in parsing are ignored
-                    ),
-                    _ => (), // ignores unrecognised tags
-                },
-            },
-            Err(_) => return Err(LrcParseError::NoTagInNonEmptyLine(i)),
+pub fn parse<'a>(
+    lyric_lines: impl Iterator<Item = &'a str>,
+) -> Result<Vec<LrcItem<'a>>, LrcParseError> {
+    let mut lrc_tags = Vec::new();
+
+    for (line_num, line) in lyric_lines.enumerate() {
+        if let Some(tag) = parse_single(line, line_num)? {
+            lrc_tags.push(tag);
         }
     }
 
-    Ok(lrc_items)
+    Ok(lrc_tags)
 }
